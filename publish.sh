ea599e5b1f71ed09283a7aab555b642c769c9aae
Emilien Andre <emilien.andre2764@gmail.com>
2023-04-14 23:01:33 +0200

    first commit

---
+++ publish.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+GMAD=~/.local/share/Steam/steamapps/common/GarrysMod/bin/gmad_linux
+GMPUBLISH=~/.local/share/Steam/steamapps/common/GarrysMod/bin/gmpublish_linux
+LD_LIBRARY_PATH=~/.local/share/Steam/steamapps/common/GarrysMod/bin/
+export LD_LIBRARY_PATH
+$GMAD create -folder . -out ../sabal_palmetto.gma
+$GMPUBLISH create -addon ../sabal_palmetto.gma -icon icon.jpg
+rm ../sabal_palmetto.gma
---
+++ update.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+GMAD=~/.local/share/Steam/steamapps/common/GarrysMod/bin/gmad_linux
+GMPUBLISH=~/.local/share/Steam/steamapps/common/GarrysMod/bin/gmpublish_linux
+LD_LIBRARY_PATH=~/.local/share/Steam/steamapps/common/GarrysMod/bin/
+export LD_LIBRARY_PATH
+$GMAD create -folder . -out ../sabal_palmetto.gma
+$GMPUBLISH update -addon ../sabal_palmetto.gma -id 2932685055 -icon icon.jpg
+rm ../sabal_palmetto.gma
